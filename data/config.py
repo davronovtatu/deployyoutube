5a95ac949a1f64f5fb90c8c893a4a0f329b3a140
DAVRONOV_UZB <davronovtatu@gmail.com>
2023-05-11 19:45:53 +0500

    birinchi ozgarish

---
+++ data/config.py
@@ -0,0 +1,18 @@
+# from environs import Env
+#
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+#
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+
+
+BOT_TOKEN = "6284604962:AAEz_SUHlBO5K6jatcDEN2-VPFMQTJ3_Ayg" # Bot toekn
+ADMINS = [1962258253]  # adminlar ro'yxati
+IP = "localhost"  # Xosting ip manzili
+
+
+
